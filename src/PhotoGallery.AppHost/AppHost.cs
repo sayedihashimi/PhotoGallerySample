27b2a2d0eb129da6690cc106645ce5246b794411
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-25 11:34:25 -0400

    commented out code to get back to starting point

--- src/PhotoGallery.AppHost/AppHost.cs
+++ src/PhotoGallery.AppHost/AppHost.cs
@@ -1,14 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApplication1>("webapp1");
+//builder.AddProject<Projects.WebApplication1>("webapp1");
 
-var photos = builder.AddAzureStorage("storage")
-                .RunAsEmulator()
-                .AddBlobs("blobs")
-                .AddBlobContainer("photos");
+//var photos = builder.AddAzureStorage("storage")
+//                .RunAsEmulator()
+//                .AddBlobs("blobs")
+//                .AddBlobContainer("photos");
 
-builder.AddProject<Projects.WebApplication1>("webapp")
-        .WithReference(photos)
-        .WaitFor(photos);
+//builder.AddProject<Projects.WebApplication1>("webapp")
+//        .WithReference(photos)
+//        .WaitFor(photos);
 
 builder.Build().Run();
--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,39 +1,47 @@
-using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Http.HttpResults;
-using WebApplication1.Components;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.AddServiceDefaults();
-builder.AddAzureBlobContainerClient("photos");
-builder.Services.AddRazorComponents();
-builder.Services.AddAntiforgery();
-
 var app = builder.Build();
-app.MapDefaultEndpoints();
-app.UseAntiforgery();
 
-app.MapGet("/", async (BlobContainerClient client) =>
-{
-    var blobs = client.GetBlobsAsync();
-    var photos = new List<string>();
-    await foreach (var photo in blobs)
-    {
-        photos.Add(photo.Name);
-    }
-    return new RazorComponentResult<PhotoList>(new { Photos = photos });
-});
-
-app.MapPost("/upload", async (IFormFile photo, BlobContainerClient client) =>
-{
-    if (photo.Length > 0)
-    {
-        var blobClient = client.GetBlobClient(photo.FileName);
-        await blobClient.UploadAsync(photo.OpenReadStream(), true);
-    }
-
-    return Results.Redirect("/");
-});
-
-
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+//using Azure.Storage.Blobs;
+//using Microsoft.AspNetCore.Http.HttpResults;
+//using WebApplication1.Components;
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.AddServiceDefaults();
+//builder.AddAzureBlobContainerClient("photos");
+//builder.Services.AddRazorComponents();
+//builder.Services.AddAntiforgery();
+
+//var app = builder.Build();
+//app.MapDefaultEndpoints();
+
+//app.UseAntiforgery();
+
+//app.MapGet("/", async (BlobContainerClient client) =>
+//{
+//    var blobs = client.GetBlobsAsync();
+//    var photos = new List<string>();
+//    await foreach (var photo in blobs)
+//    {
+//        photos.Add(photo.Name);
+//    }
+//    return new RazorComponentResult<PhotoList>(new { Photos = photos });
+//});
+
+//app.MapPost("/upload", async (IFormFile photo, BlobContainerClient client) =>
+//{
+//    if (photo.Length > 0)
+//    {
+//        var blobClient = client.GetBlobClient(photo.FileName);
+//        await blobClient.UploadAsync(photo.OpenReadStream(), true);
+//    }
+
+//    return Results.Redirect("/");
+//});
+
+
+
+//app.Run();
